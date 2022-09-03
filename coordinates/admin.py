e80c8ddd4f9fa3600e8a17c7670e2a8530ae4b82
Hariom Tiwari <hariom.tiwari@pasarpolis.co.id>
2022-09-04 00:02:42 +0530

    Register models in admin

--- coordinates/admin.py
+++ coordinates/admin.py
@@ -1,3 +1,8 @@
+from coordinates.models import PointsData, UserGroup, GroupLeader, GroupMembers
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(GroupLeader)
+admin.site.register(GroupMembers)
+admin.site.register(PointsData)
+admin.site.register(UserGroup)
