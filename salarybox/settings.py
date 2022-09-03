b3b378b4189a66604520c9a354f1ec23e7351a07
Hariom Tiwari <hariom.tiwari@pasarpolis.co.id>
2022-09-03 23:34:02 +0530

    Add django extensions

--- salarybox/settings.py
+++ salarybox/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third apps
+    'rest_framework',
+    'rest_framework.authtoken',
+    'django_extensions',
+
+    # Custom django apps
+    'coordinates',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +129,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'PAGE_SIZE': 100,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication'
+    ),
+}
