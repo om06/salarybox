e89ee716a623cb21da58f927f9caf8045958ef1b
Hariom Tiwari <hariom.tiwari@pasarpolis.co.id>
2022-09-05 01:58:24 +0530

    Fix bug in group api

--- coordinates/api/v1_urls.py
+++ coordinates/api/v1_urls.py
@@ -1,10 +1,10 @@
 from django.urls import include, re_path
 from rest_framework import routers
-from coordinates.views import GraphViewSet, LoadFileViewSet, PointsDataViewSet
+from coordinates.views import GraphViewSet, GroupDataViewSet, LoadFileViewSet, PointsDataViewSet
 
 router = routers.DefaultRouter()
 router.register('points', PointsDataViewSet, basename='points')
-router.register('group', LoadFileViewSet, basename='group')
+router.register('group', GroupDataViewSet, basename='group')
 router.register('graph', GraphViewSet, basename='graph')
 router.register('csv', LoadFileViewSet, basename='csv')
 
